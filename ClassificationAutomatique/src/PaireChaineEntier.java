059c9473c6c59d75ee019530b67cab5dd4b87f8f
FAURE Lukas <faurelu@hostname.iut2.upmf-grenoble.fr>
2024-01-15 13:34:00 +0100

    test

--- ClassificationAutomatique/src/PaireChaineEntier.java
+++ ClassificationAutomatique/src/PaireChaineEntier.java
@@ -1,3 +1,22 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PaireChaineEntier {
-    private static String test= "test";
+    private String chaine;
+    private int entier;
+
+    public PaireChaineEntier(String chaine, int entier){
+        this.chaine = chaine;
+        this.entier = entier;
+    }
+
+    public String getChaine(){
+        return chaine;
+    }
+
+    public int getEntier(){
+        return entier;
+    }
 }
