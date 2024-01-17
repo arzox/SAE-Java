bd00f3bd3c7f2a369a22a6c60f85919ea8b01641
FAURE Lukas <faurelu@hostname.iut2.upmf-grenoble.fr>
2024-01-17 08:33:04 +0100

    docStringPart2

--- ClassificationAutomatique Part2/src/UtilitairePaireChaineEntier.java
+++ ClassificationAutomatique Part2/src/UtilitairePaireChaineEntier.java
@@ -5,7 +5,14 @@ import java.util.TreeMap;
 
 public class UtilitairePaireChaineEntier {
 
-
+    /**
+     * Recherche une clé (mot) dans une Map et renvoie la clé correspondante
+     * à partir d'une chaîne donnée, en utilisant une recherche binaire adaptée aux préfixes.
+     *
+     * @param treeMap Le TreeMap contenant les mots en tant que clés.
+     * @param chaine La chaîne pour laquelle trouver la clé correspondante.
+     * @return La clé correspondante à la chaîne spécifiée, ou une chaîne vide si aucune correspondance n'est trouvée.
+     */
     public static String keyFromWord(Map<String, Integer> treeMap, String chaine) {
         ArrayList<String> listePaires = new ArrayList<>(treeMap.keySet());
         int debut = 0;
@@ -25,6 +32,14 @@ public class UtilitairePaireChaineEntier {
         return "";
     }
 
+
+    /**
+     * Obtient la valeur associée à une chaîne dans un TreeMap, en utilisant une recherche de clé.
+     *
+     * @param MapPaires La Map contenant les paires (clé, valeur).
+     * @param chaine La chaîne pour laquelle obtenir la valeur associée.
+     * @return La valeur associée à la chaîne spécifiée, ou 0 si aucune correspondance n'est trouvée.
+     */
     public static int valueFromWord(Map<String, Integer> MapPaires, String chaine) {
         String key = keyFromWord(MapPaires, chaine);
         return key.isEmpty() ? 0 : MapPaires.get(key);
--- ClassificationAutomatique Part2/src/UtilitaireWrite.java
+++ ClassificationAutomatique Part2/src/UtilitaireWrite.java
@@ -4,6 +4,13 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class UtilitaireWrite {
+    /**
+     * Écrit le contenu en parametres dans le fichier spécifié
+     *
+     * @param nomFichier Le chemin du fichier dans lequel écrire le contenu
+     * @param contenu Le contenu à écrire dans le fichier
+     * @throws RuntimeException Si une erreur d'entrée/sortie survient lors de l'écriture dans le fichier
+     */
     public static void write(String nomFichier, String contenu) {
         try {
             Files.writeString(Paths.get(nomFichier), contenu, StandardOpenOption.APPEND);
@@ -12,6 +19,12 @@ public class UtilitaireWrite {
         }
     }
 
+    /**
+     * Supprime tout le contenu dans le fichier spécifié
+     *
+     * @param nomFichier Le chemin du fichier dans lequel supprimer le contenu
+     * @throws RuntimeException Si une erreur d'entrée/sortie survient lors de l'écriture dans le fichier
+     */
     public static void clear(String nomFichier) {
         PrintWriter writer = null;
         try {
@@ -23,6 +36,11 @@ public class UtilitaireWrite {
         writer.close();
     }
 
+    /**
+     * Permet de crée un fichier avec un nom spécifié
+     *
+     * @param nomFichier Le chemin où crée le fichier avec son nom
+     */
     public static void createFile(String nomFichier){
         File file = new File(nomFichier);
     }
